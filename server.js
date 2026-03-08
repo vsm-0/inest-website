c04202de721cf7057a91053aabd9f7256658a9b8
vsm-0 <166985393+vsm-0@users.noreply.github.com>
2026-03-08 19:52:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,19 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch(err => console.error(err));
+console.log("Starting server...");
+console.log("Mongo URI:", process.env.MONGO_URI);
 
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB connected");
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+})
+.catch(err => {
+  console.error("MongoDB connection error:");
+  console.error(err);
+});
