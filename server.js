1cc2e7be2ebe7b2c9b7f7943e957e033820abeb1
vsm-0 <166985393+vsm-0@users.noreply.github.com>
2026-03-08 19:37:41 +0530

    Update server log message to include port number

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
     app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch(err => console.error(err));
+
